eae77b9a9fe47d0b8ddaf2f98879690cebb8a99a
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-01-29 16:08:18 +0100

    work in progress

--- healthy-eating/src/components/account/Account.jsx
+++ healthy-eating/src/components/account/Account.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
-const AccouontPage = () => (
-    <div>
-        <Header />
-            <h3>User Profile</h3>
-            <AccountForm />
-        <Footer />
-    </div>
-);
-  
-function AccountForm () { 
-  return (
-        <section className='account-section'>
-            <div className="menu">
-                <ul>
-                    <li>Profile</li>
-                    <li>Change Password</li>
-                    <li>My Recipes</li>
-                    <li>More Info</li>
-                </ul>
-            </div>
-            <div className="main-section">
-                <h2>Some information about the account</h2>
-            </div>
-        </section>  
+function Account () {
+    
+    return (
+        <Fragment>
+            <Header />
+                <section className='account-section'>
+                    <div className="menu">
+                        <ul>
+                            <li>Profile</li>
+                            <li>Change Password</li>
+                            <li>My Recipes</li>
+                            <li>More Info</li>
+                        </ul>
+                    </div>
+                    <div className="main-section">
+                        <h2>Some information about the account</h2>
+                    </div>
+                </section>
+            <Footer />
+        </Fragment>     
     );
 }
 
-export default AccouontPage;
+export default Account;
