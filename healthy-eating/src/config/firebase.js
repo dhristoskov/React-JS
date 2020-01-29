44f6eb76b30546a1af20221fb9c7cdaf947d8a67
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-01-29 16:04:58 +0100

    work in progress

--- healthy-eating/src/config/firebase.js
+++ healthy-eating/src/config/firebase.js
@@ -1,70 +1,63 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/firestore'
+import 'firebase/firestore';
+import "firebase/storage";
 
 //Firebase Authenticaion Data
 const firebaseConfig = {
 
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCMJgvEYT0SwIs3sbrfpF_N-PhDIfwZ0AI",
+    authDomain: "healthy-eating-77d37.firebaseapp.com",
+    databaseURL: "https://healthy-eating-77d37.firebaseio.com",
+    projectId: "healthy-eating-77d37",
+    storageBucket: "healthy-eating-77d37.appspot.com",
+    messagingSenderId: "557664417140",
+    appId: "1:557664417140:web:82bc40b67c819dea362821"
 
 };
 
 class Firebase {
-
-    constructor(){
-        firebase.initializeApp(firebaseConfig);
-        this.auth = firebase.auth();
-        this.dataBase = firebase.firestore();
-    };
-
-
-    //Login Helper
-    login(email, password) {
-		return this.auth.signInWithEmailAndPassword(email, password)
+	constructor() {
+		firebase.initializeApp(firebaseConfig)
+		this.auth = firebase.auth()
+		this.db = firebase.firestore()
 	}
 
-    //Logout Helper
-	logout() {
-		return this.auth.signOut()
+	//LogIn Helper
+	async login(email, password){
+        const user = await firebase.auth().signInWithEmailAndPassword(email, password).catch(err => {
+            //console.log(err);
+            return err;
+        });
+        return user;
 	}
 
-    //Register Helper
-	async register(name, email, password) {
-		await this.auth.createUserWithEmailAndPassword(email, password)
-		return this.auth.currentUser.updateProfile({
-			displayName: name
-		})
-    }
-    
-    //Reset Password Helper
-    passwordReset (email) {
-        return this.auth.sendPasswordResetEmail(email);
+	//Logout Helper
+	async logout(){
+        const logout = await firebase.auth().signOut().catch(err => {
+            //console.log(err);
+            return err;
+        });
+        return logout;
     }
 
-    //Update Password Helper
-    passwordUpdate (password) {
-        return this.auth.currentUser.updatePassword(password);
-    }  
+	//Registration Helper
+	async register(email, password){
+		const user = await firebase.auth().createUserWithEmailAndPassword(email, password).catch(err => {
+            //console.log(err);
+            return err;
+        });
+        return user;
+    }
 
-    //Authenticate user
-	isInitialized() {
-		return new Promise(resolve => {
-			this.auth.onAuthStateChanged(resolve)
-		})
-	}
 
-    //Display User Name
-	getCurrentUsername() {
-		return this.auth.currentUser && this.auth.currentUser.displayName
-	}
-
+	async getUserState(){
+        return new Promise(resolve=> {
+            this.auth.onAuthStateChanged(resolve);
+        });
+    }
 }
 
 export default new Firebase()
+
   
--- healthy-eating/src/constants/routes.js
+++ healthy-eating/src/constants/routes.js
@@ -1,7 +1,6 @@
 export const LANDING = '/';
-export const REGISTER = '/register';
-export const LOGIN = '/login';
+export const REGISTER = 'register';
+export const LOGIN = 'login';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
