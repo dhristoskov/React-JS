4f7c847de3ba5c17a077747b065bcefa1789d093
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-01-30 22:08:46 +0100

    work in progress

--- healthy-eating/src/index.js
+++ healthy-eating/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './components/context/authContext';
 import { BrowserRouter } from 'react-router-dom';
+import { RecipesProvider } from './components/context/recipeContext';
 
 ReactDOM.render(
     <BrowserRouter>
         <AuthProvider>
+            <RecipesProvider>
                 <App />
+            </RecipesProvider>         
         </AuthProvider>
     </BrowserRouter>  
 ,document.getElementById('root'));
