d2217fe0b1aa84c009b382ac2a296abfdee3738c
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-01-30 22:09:43 +0100

    work in progress

--- healthy-eating/src/config/firebase.js
+++ healthy-eating/src/config/firebase.js
@@ -6,21 +6,23 @@ import "firebase/storage";
 //Firebase Authenticaion Data
 const firebaseConfig = {
 
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCMJgvEYT0SwIs3sbrfpF_N-PhDIfwZ0AI",
+    authDomain: "healthy-eating-77d37.firebaseapp.com",
+    databaseURL: "https://healthy-eating-77d37.firebaseio.com",
+    projectId: "healthy-eating-77d37",
+    storageBucket: "healthy-eating-77d37.appspot.com",
+    messagingSenderId: "557664417140",
+    appId: "1:557664417140:web:82bc40b67c819dea362821"
 
 };
 
 class Firebase {
+
 	constructor() {
 		firebase.initializeApp(firebaseConfig)
 		this.auth = firebase.auth()
-		this.db = firebase.firestore()
+        this.db = firebase.firestore()
+        this.storage = firebase.storage();
 	}
 
 	//LogIn Helper
@@ -43,18 +45,92 @@ class Firebase {
 
 	//Registration Helper
 	async register(email, password){
-		const user = await firebase.auth().createUserWithEmailAndPassword(email, password).catch(err => {
-            //console.log(err);
-            return err;
+        const user = await firebase.auth()
+        .createUserWithEmailAndPassword(email, password).catch(err => {
+        return err;
         });
         return user;
     }
 
+    //Get a list with all recipes from the storage
+    async getRecipes(){
+        let recipesList = [];
+        const recipes = await firebase.firestore().collection("recipes").get();
+        recipes.forEach(doc => {
+            recipesList.push({id:doc.id, data: doc.data()});
+        });
+        return recipesList;
+    }
 
+
+    //Get a single recipe from the storage
+    async getRecipe(recipeId){
+        const recipe = await firebase.firestore().collection("recipes").doc(recipeId).get();
+        const recipeData = recipe.data();
+        return recipeData;
+    }
+
+    //Authentication
 	async getUserState(){
         return new Promise(resolve=> {
             this.auth.onAuthStateChanged(resolve);
         });
+    }
+
+    //Add a new Recipe in the storage
+    async createRecipe(url, recipe){
+   
+        const fileRef = await firebase.storage().refFromURL(url);
+    
+        let newRecipe = {
+            title: recipe.title,
+            content: recipe.content,
+            cover: url,
+            fileref : fileRef.location.path 
+        } 
+        
+        const firestoreRecipe = await firebase.firestore()
+        .collection("recipes").add(newRecipe).catch(err => {
+            console.log(err);
+        });  
+        return firestoreRecipe;     
+    }
+
+    //Update current Recipe data in storage
+    async updateRecipe(url, id, data){
+        if(data["cover"]){
+          const fileRef = await firebase.storage().refFromURL(url);
+          await this.storage.ref().child(data["oldcover"]).delete().catch(err => {
+          });
+  
+          let updatedData = {
+              title: data.title,
+              content: data.content,
+              cover: url,
+              fileref : fileRef.location.path
+          }
+  
+          const recipe = await firebase.firestore().collection("recipes")
+          .doc(id).set(updatedData, {merge: true}).catch(err => {            
+          });
+          return recipe;
+        }else{
+          const recipe = await firebase.firestore().collection("recipes")
+          .doc(id).set(data, {merge: true}).catch(err => {
+          });
+          return recipe
+        }
+     }
+
+    //Delete current Recipe from storage
+    async deleteRecipe(id, fileref){
+        const storageRef = firebase.storage().ref();
+        await storageRef.child(fileref).delete().catch(err => {
+        });
+        const recipe = await firebase.firestore().collection("recipes")
+        .doc(id).delete().catch(err => {
+        });
+        return recipe;
     }
 }
 
