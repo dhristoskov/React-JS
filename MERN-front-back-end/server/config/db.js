b103df9ee663871d6b55657612bd0cfc521aad14
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-02-11 21:23:39 +0100

    create db

---
+++ MERN-front-back-end/server/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const db = process.env.ATLAS_URI
+
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    console.log('Connected to MongoDB successfuly...')
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+}
+
+module.exports = ConnectDB;
