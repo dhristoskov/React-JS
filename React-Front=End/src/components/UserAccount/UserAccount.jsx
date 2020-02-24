947fee1263c945c2cccf60dc558c8a88c4c278ea
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2020-02-24 18:58:29 +0100

    commit

---
+++ React-Front=End/src/components/UserAccount/UserAccount.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import ReservationForm from '../Reservation/ReservationForm';
+
+const UserAccount = () => {
+    return(
+        <Fragment>
+            <div>User Account</div>
+            <ReservationForm />
+        </Fragment>
+    );
+};
+
+export default UserAccount;
